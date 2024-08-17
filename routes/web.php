374591b96de16dffa11a3fd89cf4c0195150afde
Ryugudouji <golden83caner@yahoo.co.jp>
2024-08-18 00:42:44 +0900

    27章

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 // ルーティングを設定するコントローラを宣言する
 use App\Http\Controllers\HelloController;
@@ -25,6 +26,18 @@ use App\Http\Controllers\SessionController;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
 
 Route::get('/hello', [HelloController::class, 'index']);
 
@@ -67,3 +80,4 @@ Route::get('/sessions/create', [SessionController::class, 'create'])->name('sess
 Route::post('/sessions/store', [SessionController::class, 'store'])->name('sessions.store');
 
 Route::delete('/sessions/destroy', [SessionController::class, 'destroy'])->name('sessions.destroy');
+
